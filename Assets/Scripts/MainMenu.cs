a80de34b29c639cd3b1540ee3ed2e8ac6ad7cfe5
marvel-natanael <63591046+marvel-natanael@users.noreply.github.com>
2021-11-13 22:08:59 +0700

    audio setup

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     private static bool isShown = false;
-    public Button creditsObj;
-    public Button settingsObj;
+    private bool isMusicOn, isSfxOn;
+    public Button musicButton, sfxButton;
+    public Sprite musicOn, musicOff, sfxOn, sfxOff;
+    public GameObject creditsObj;
+    public GameObject settingsObj;
+    public AudioMixer sfxMixer, musicMixer;
 
     private void Start()
     {
@@ -21,16 +26,47 @@ public class MainMenu : MonoBehaviour
     }
     public void setSFX()
     {
-
-    }
-    public void setMusic()
-    {
-
+        isSfxOn = !isSfxOn;
+        switch (isSfxOn)
+        {
+            case true:
+                {
+                    sfxMixer.SetFloat("sfxVolume", -80);
+                    sfxButton.GetComponent<Image>().sprite = sfxOff;
+                    break;
+                }
+            case false:
+                {
+                    sfxMixer.SetFloat("sfxVolume", 0);
+                    sfxButton.GetComponent<Image>().sprite = sfxOn;
+                    break;
+                }
+        }
     }
     public void showCredits()
     {
         isShown = !isShown;
         creditsObj.gameObject.SetActive(isShown);
         settingsObj.gameObject.SetActive(false);
+    }
+
+    public void setMusic()
+    {
+        isMusicOn = !isMusicOn;
+        switch (isMusicOn)
+        {
+            case true:
+                {
+                    musicMixer.SetFloat("musicVolume", -80);
+                    musicButton.GetComponent<Image>().sprite = musicOff;
+                    break;
+                }
+            case false:
+                {
+                    musicMixer.SetFloat("musicVolume", 0);
+                    musicButton.GetComponent<Image>().sprite = musicOn;
+                    break;
+                }
+        }
     }
 }
